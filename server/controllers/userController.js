76574caa48bdd3ae3da23d3193e6023c311001fb
savioshaju <savioshajum@gmail.com>
2025-10-31 21:28:55 +0530

    cookie issue

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,7 +34,7 @@ const signup = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'PRODUCTION',
-            sameSite: 'Strict',
+            sameSite: 'none',
             maxAge: 2 * 24 * 60 * 60 * 1000
         });
 
@@ -88,7 +88,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'PRODUCTION',
-            sameSite: 'Strict',
+            sameSite: 'none',
             maxAge: 2 * 24 * 60 * 60 * 1000
         });
 
