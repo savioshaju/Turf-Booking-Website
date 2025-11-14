01742d9b85222d48603d3d318e34d585db141e32
savioshaju <savioshajum@gmail.com>
2025-11-14 21:05:53 +0530

    login icon

--- client/src/Components/User/NavBar.jsx
+++ client/src/Components/User/NavBar.jsx
@@ -62,8 +62,7 @@ function NavBar() {
       label: 'Login',
       icon: (
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-            d="M15 3h4a2 2 0 012 2v4m0 0l-6-6m6 6l-6 6M9 21H5a2 2 0 01-2-2v-4m0 0l6 6m-6-6l6-6" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
         </svg>
       ),
       tooltip: 'Login',
