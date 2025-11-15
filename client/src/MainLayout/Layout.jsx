4d3b08ee81b30560cecb6fdc4c7870eac77387f6
savioshaju <savioshajum@gmail.com>
2025-11-15 11:26:46 +0530

    Login

--- client/src/MainLayout/Layout.jsx
+++ client/src/MainLayout/Layout.jsx
@@ -2,12 +2,38 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../Components/User/NavBar';
 import Footer from '../Components/User/Footer';
-
+import { useDispatch, useSelector } from 'react-redux';
+import axiosInstance from '../Config/axiosInstance';
+import { clearUserData, saveUserData } from '../store/slice/userSlice';
+import { useState,useEffect
+  
+ } from 'react';
 const Layout = () => {
   const location = useLocation();
+  const dispatch = useDispatch()
+  const { userData } = useSelector(state => state.user)
+  const [loading, setLoading] = useState(true)
 
-  const mainClasses = location.pathname === '/' 
-    ? 'min-h-[50vh] ' 
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  function checkUser() {
+    axiosInstance.get('/user/check-user')
+      .then((res) => {
+        dispatch(saveUserData(res.data.data));
+        setLoading(false);
+      })
+      .catch((err) => {
+        dispatch(clearUserData());
+        setLoading(false);
+      });
+  }
+
+
+  const mainClasses = location.pathname === '/'
+    ? 'min-h-[50vh] '
     : 'min-h-[50vh] pt-24';
 
   return (
